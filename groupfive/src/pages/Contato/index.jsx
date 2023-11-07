e8146a7967f0dd18e6b2289da721a5cb8f356aa5
rblfa <digoberredo@gmail.com>
2023-11-07 16:31:02 -0300

    Notificação de mensagem enviada na página de contato

--- groupfive/src/pages/Contato/index.jsx
+++ groupfive/src/pages/Contato/index.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import HeaderMain from '../../components/Header'
 import Footer from '../../components/Footer'
 import './style.css'
 
 export default function Contato() {
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [telefone, setTelefone] = useState('');
+    const [assunto, setAssunto] = useState('');
+    const [mensagem, setMensagem] = useState('');
+    const [mensagemNotificacao, setMensagemNotificacao] = useState('');
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (nome && email && telefone && assunto && mensagem) {
+            setMensagemNotificacao('Mensagem enviada com sucesso!');
+            setNome('');
+            setEmail('');
+            setTelefone('');
+            setAssunto('');
+            setMensagem('');
+        } else {
+            setMensagemNotificacao('Por favor, preencha todos os campos.');
+        }
+    };
     return (
         <>
             <HeaderMain />
             <main className="contato">
                 <h2>CONTATO</h2>
                 <div className="card-body-post">
-                    <form action="#">
+                    <form onSubmit={handleSubmit}>
                         <div className="fields">
                             <label htmlFor="nome">Nome: </label>
                             <input
@@ -18,6 +40,8 @@ export default function Contato() {
                                 type="text"
                                 id="nome"
                                 name="nome"
+                                value={nome}
+                                onChange={(e) => setNome(e.target.value)}
                             />
                         </div>
                         <div className="fields">
@@ -27,6 +51,8 @@ export default function Contato() {
                                 type="email"
                                 id="email"
                                 name="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
                             />
                         </div>
                         <div className="fields">
@@ -36,6 +62,8 @@ export default function Contato() {
                                 type="text"
                                 id="telefone"
                                 name="telefone"
+                                value={telefone}
+                                onChange={(e) => setTelefone(e.target.value)}
                             />
                         </div>
                         <div className="fields">
@@ -45,15 +73,30 @@ export default function Contato() {
                                 type="text"
                                 id="assunto"
                                 name="assunto"
+                                value={assunto}
+                                onChange={(e) => setAssunto(e.target.value)}
                             />
                         </div>
                         <div className="fields">
                             <label htmlFor="mensagem">Mensagem: </label>
-                            <textarea name="mensagem" id="mensagem" cols="30" rows="10" placeholder="Mensagem"></textarea>
+                            <textarea
+                                name="mensagem"
+                                id="mensagem"
+                                cols="30"
+                                rows="10"
+                                placeholder="Mensagem"
+                                value={mensagem}
+                                onChange={(e) => setMensagem(e.target.value)}
+                            ></textarea>
                         </div>
-                        <button>
-                            Enviar
-                        </button>
+                        <div className='botaoEnviar'>
+                            <button type='submit'>
+                                Enviar
+                            </button>
+                        </div>
+                        <div className='notificacao'>
+                            {mensagemNotificacao}
+                        </div>
                     </form>
                 </div>
             </main>
